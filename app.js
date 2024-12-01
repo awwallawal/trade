73dc8f94914349a51d662645e5a75d5ad4e27720
Awwal <awwallawal@gmail.com>
2024-12-01 17:43:54 +0100

    Initial File Creation

---
+++ app.js
@@ -0,0 +1,6 @@
+const btnCta = document.querySelector("#btn_cta");
+console.log(btnCta);
+btnCta.addEventListener("click", ()=> {
+    // window.location.href = https://www.nairaland.com/
+    window.open("https://www.nairaland.com/", "_blank")
+})
